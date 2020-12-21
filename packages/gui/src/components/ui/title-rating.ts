293622f4777a1f704f0cad67aedc0c17178d9110
erikvullings <erik.vullings@gmail.com>
2020-12-21 11:31:48 +0100

    Wrap in row/col

--- packages/gui/src/components/ui/title-rating.ts
+++ packages/gui/src/components/ui/title-rating.ts
@@ -6,13 +6,13 @@ export const TitleRating: FactoryComponent<{ content: Partial<IContent> }> = () 
     view: ({ attrs: { content } }) => {
       const { date, title, rating, author } = content;
       const d = date ? '<br>' + new Date(date).toLocaleDateString('nl') : '';
-      return [
+      return m('.row', [
         m(
           'h3.center-align',
           m.trust(`${title}${rating ? ` (${rating}<span style="color: gold">&#9733;</span>)` : ''}`)
         ),
         author && m('b.col.s12.center-align', m.trust(`door ${author}${d}`)),
-      ];
+      ]);
     },
   };
 };
