ba8526dc111f9cec17cf36bc8b58a509a08a0f2d
erikvullings <erik.vullings@gmail.com>
2020-12-21 11:32:38 +0100

    Adjust layout

--- packages/gui/src/components/collections/scenarios/scenario-view.ts
+++ packages/gui/src/components/collections/scenarios/scenario-view.ts
@@ -48,57 +48,67 @@ export const ScenarioView: MeiosisComponent = () => {
       const showAnswer = m.route.param('toon');
 
       return [
-        m(
-          '.item-view',
+        m('.scenario-view', [
           m('.row', [
-            m(TitleRating, { content: current }),
-            m(ImageBox, { content: current }),
-            m('.col.s12', m(SlimdownView, { md: desc })),
-            [
+            m('.col.s12.m6', [
+              m(TitleRating, { content: current }),
+              m(ImageBox, { content: current }),
+              m(SlimdownView, { md: desc }),
+            ]),
+            m('.col.s12.m6', [
               phases.length > 1 &&
-                m(
-                  '.col.s12',
-                  {
-                    key: 'pager',
-                  },
-                  m(Pagination, {
-                    size: 10,
-                    className: 'col s12',
-                    curPage: fase + 1,
-                    items: phases.map((_p, i) => ({ href: `/draaiboek/${current.$loki}?fase=${i}` })),
-                  })
-                ),
-              phase &&
-                m('div', { key: Date.now() }, [
-                  m('h4', `Fase ${fase + 1}: ${phase.title}`),
-                  m(ImageBox, { content: phase }),
-                  m('.col.s12', m(SlimdownView, { md: phase.desc })),
-                  phase.dilemmas && [
-                    m('h5', 'Vragen'),
-                    phase.dilemmas.map((d) =>
-                      m('.col.s12', [
-                        d.title && m(SlimdownView, { md: '* ' + d.title }),
-                        d.desc && m(SlimdownView, { md: d.desc }),
-                        showAnswer && d.notes && m(SlimdownView, { md: '> ' + d.notes }),
-                      ])
-                    ),
-                    m(FlatButton, {
-                      className: 'right-align',
-                      label: showAnswer ? 'Verberg de keuzes' : 'Toon gemaakte keuzes',
-                      onclick: () =>
-                        changePage(Dashboards.SCENARIOS_VIEW, { id }, showAnswer ? { fase } : { fase, toon: 'true' }),
-                    }),
-                  ],
+                m('.row', [
+                  phase &&
+                    m('.col.s12', [
+                      m('h3', `Fase ${fase + 1}: ${phase.title}`),
+                      m(ImageBox, { content: phase }),
+                      m(SlimdownView, { md: phase.desc }),
+                      phase.dilemmas && [
+                        m('h4', 'Vragen'),
+                        phase.dilemmas.map((d) =>
+                          m('.col.s12', [
+                            d.title && m(SlimdownView, { md: '* ' + d.title }),
+                            d.desc && m(SlimdownView, { md: d.desc }),
+                            showAnswer && d.notes && m(SlimdownView, { md: '> ' + d.notes }),
+                          ])
+                        ),
+                        m(FlatButton, {
+                          className: 'right-align',
+                          label: showAnswer ? 'Verberg de keuzes' : 'Toon gemaakte keuzes',
+                          onclick: () =>
+                            changePage(
+                              Dashboards.SCENARIOS_VIEW,
+                              { id },
+                              showAnswer ? { fase } : { fase, toon: 'true' }
+                            ),
+                        }),
+                      ],
+                    ]),
+                  m(
+                    '.col.s12.center-align',
+                    m(Pagination, {
+                      size: 10,
+                      className: 'col s9 offset-s3',
+                      curPage: fase + 1,
+                      items: phases.map((_p, i) => ({ href: `/draaiboek/${current.$loki}?fase=${i}` })),
+                    })
+                  ),
                 ]),
-            ],
-          ])
-        ),
-        m(ViewFooter, {
-          content: current,
-          edit: Dashboards.SCENARIOS_EDIT,
-          changePage,
-          save,
-        }),
+            ]),
+          ]),
+          m(
+            '.row',
+            m(
+              '.col.s11.center-align.bottom',
+              m(ViewFooter, {
+                content: current,
+                edit: Dashboards.SCENARIOS_EDIT,
+                changePage,
+                save,
+              })
+            )
+          ),
+        ]),
       ];
     },
   };
